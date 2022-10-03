c3cccd8ad534540605ac2cee7ccb8ba9451ef2f1
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-10-04 01:29:09 +0500

    JS Beginner to Professional practise ch01

---
+++ Quarter I-B Object Oriented Programming using Typescript/01 learning javaScript/b - JavaScript from Beginner to Professional/01 Getting Started with JS/index1.js
@@ -0,0 +1,6 @@
+// Display data in the console
+console.log('Hello world!');
+// Display data in tabluar form
+console.table(['apples', 'oranges', 'bananas']);
+// Display data in error form
+console.error('Hello', 'World', '!');
