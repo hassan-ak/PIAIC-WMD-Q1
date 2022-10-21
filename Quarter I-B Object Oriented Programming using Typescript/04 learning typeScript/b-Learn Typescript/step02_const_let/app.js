1274ac52686a1e5abdd27b72b1e7a291d197df0e
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-10-22 01:47:33 +0500

    TypeScript Repo step02

---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step02_const_let/app.js
@@ -0,0 +1,17 @@
+"use strict";
+//use const where variable values do not change
+const a = 5;
+const b = 33;
+const c = 'best';
+//I suggest use let instead of var everywhere, becuase let has blocked scope
+if (true) {
+    let z = 4;
+    //use z
+}
+else {
+    let z = 'string';
+    //use z
+}
+/*
+console.log("let: " + z);// Error: z is not defined in this scope
+*/
