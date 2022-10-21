2e898e0cfa2239f66a34d9c9d7a7dcb8cc75449d
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-10-22 01:49:56 +0500

    TypeScript Repo step14

---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step14_callbacks_typed/app.js
@@ -0,0 +1,8 @@
+"use strict";
+function myCallBack(text) {
+    console.log('inside myCallback ' + text);
+}
+function callingFunction(initialText, callback) {
+    callback(initialText);
+}
+callingFunction('myText', myCallBack);
