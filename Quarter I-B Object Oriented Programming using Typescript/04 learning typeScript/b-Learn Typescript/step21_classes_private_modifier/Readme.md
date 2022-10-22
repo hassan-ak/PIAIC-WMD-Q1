024d7af33d3aa1aa04e03f1412d597d00f3f6d8e
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-10-23 00:42:11 +0500

    TypeScript Repo step21

---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step21_classes_private_modifier/Readme.md
@@ -0,0 +1,3 @@
+# TypeScript
+
+## Private Class Modifier
---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step21_classes_private_modifier/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// /In TypeScript, each member is public by default.
+class Animal {
+    constructor(theName) {
+        this.name = theName;
+    }
+    move(meters) {
+        alert(this.name + ' moved ' + meters + 'm.');
+    }
+}
+// var a = new Animal('cat').name;
+// console.log(a);
