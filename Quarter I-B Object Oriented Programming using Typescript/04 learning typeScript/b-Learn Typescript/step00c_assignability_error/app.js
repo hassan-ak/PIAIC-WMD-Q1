6bc678b1b4783c3995672362f3dab26ca2db0986
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-11-21 23:39:53 +0500

    Step-00c Assignability Error

--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step00c_assignability_error/app.js
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step00c_assignability_error/app.js
@@ -1,4 +1,5 @@
 "use strict";
+// Step-00c Assignability Error
 let message = 'Hello World';
 message = 6;
 console.log(message);
--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step00c_assignability_error/app.ts
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step00c_assignability_error/app.ts
@@ -1,3 +1,5 @@
+// Step-00c Assignability Error
+
 let message = 'Hello World';
 // message = 6;
 console.log(message);
