87612acab543b3e23ed33026e97a5090f6988f04
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-11-21 23:40:09 +0500

    Step-01 Strong Typing

--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step01_strong_typing/app.js
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step01_strong_typing/app.js
@@ -1,4 +1,5 @@
 "use strict";
+// Step-01 Strong Typing
 //strongly typed syntax
 let a = 'Pakistan';
 a = 'USA';
@@ -10,3 +11,23 @@ let f = 10.9;
 f = 22;
 let g = false;
 g = true;
+// // Types
+//
+// string
+// number
+// boolean
+// null
+// undefined
+// bigint
+// symbol
+//
+// Arrays
+// Functions
+// Objects
+// any
+//
+// Literal Types
+// Union
+// Type Aliases
+// Interfaces
+// Enums
--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step01_strong_typing/app.ts
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step01_strong_typing/app.ts
@@ -1,3 +1,5 @@
+// Step-01 Strong Typing
+
 //strongly typed syntax
 let a: string = 'Pakistan';
 a = 'USA';
@@ -10,3 +12,24 @@ let f = 10.9;
 f = 22;
 let g = false;
 g = true;
+
+// // Types
+//
+// string
+// number
+// boolean
+// null
+// undefined
+// bigint
+// symbol
+//
+// Arrays
+// Functions
+// Objects
+// any
+//
+// Literal Types
+// Union
+// Type Aliases
+// Interfaces
+// Enums
