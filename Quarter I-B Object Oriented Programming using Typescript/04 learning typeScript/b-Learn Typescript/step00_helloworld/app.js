3a67f21b04c74aaa6694f3d118d3efac54f92d86
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-11-21 23:39:06 +0500

    Step-00 Hello World

--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step00_helloworld/app.js
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step00_helloworld/app.js
@@ -1,4 +1,25 @@
 "use strict";
-let message = 'Hello World'; // Infering Types (type based on assignment),
-//take your cursor on the variable name
-console.log(message);
+// Step-00 Hello World
+// Infering Types (type based on assignment), take your cursor on the variable name
+/********************************************/
+let aString = 'Hello World';
+console.log(aString);
+/********************************************/
+let aNumber = '123';
+console.log(aNumber);
+/********************************************/
+let aBoolean = true;
+console.log(aBoolean);
+/********************************************/
+let aNull = null;
+console.log(aNull);
+/********************************************/
+let aUndefined = undefined;
+console.log(aUndefined);
+/********************************************/
+let aBigint = 5n;
+console.log(aBigint);
+/********************************************/
+let aSymbol = Symbol('Hi');
+console.log(aSymbol);
+/********************************************/
