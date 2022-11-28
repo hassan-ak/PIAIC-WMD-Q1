4488e83b4f1bdaa7cf7a3f50cf250f2890e50859
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-11-28 23:51:54 +0500

    step-08 Arrays

---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step08_arrays/app.js
@@ -0,0 +1,34 @@
+"use strict";
+// step08_arrays
+/*
+    Arrays
+*/
+//Correct Syntax
+let array1 = [5, 6, 7]; //correct syntax
+let array2 = ['1', '2', '3']; //alternative correct syntax
+let array3 = []; //correct syntax to define an empty array
+// Correct Syntax
+// let array4: number[] = new number[2](); //error
+/*
+    Arrays methods
+*/
+// indexOf()
+// lastIndexOf()
+// concat()
+// join()
+// push()
+// pop()
+// reverse()
+// shift()
+// unshift()
+// slice()
+// splice()
+// toString()
+// filter()
+// map()
+// every()
+// reduceRight()
+// reduce()
+// some()
+// sort()
+// fill()
