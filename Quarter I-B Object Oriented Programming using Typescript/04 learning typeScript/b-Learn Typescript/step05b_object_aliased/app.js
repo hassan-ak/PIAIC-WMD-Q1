3aacd603ee34054a295097c8d20fd60ed221da8d
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-12-01 04:48:42 +0500

    step05b_object_aliased

---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step05b_object_aliased/app.js
@@ -0,0 +1,18 @@
+/*-------------------------------------------------------------------*/
+// anonymous
+var teacher = {
+    name: 'Zeeshan',
+    exp: 10
+};
+var student = {
+    name: 'Hira',
+    age: 30
+};
+console.log(student['name']);
+console.log(student.age);
+var xx = { x: 100, y: 100 };
+var storeManager = {
+    name: 'Bilal'
+};
+var yy = { x: 100, y: 100 };
+/*-------------------------------------------------------------------*/
---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step05b_object_aliased/app.ts
@@ -0,0 +1,43 @@
+/*-------------------------------------------------------------------*/
+// anonymous
+let teacher: { name: string; exp: number } = {
+  name: 'Zeeshan',
+  exp: 10,
+};
+
+/*-------------------------------------------------------------------*/
+// Aliased Object Type
+type Student = {
+  name: string;
+  age?: number;
+};
+let student: Student = {
+  name: 'Hira',
+  age: 30,
+};
+console.log(student['name']);
+console.log(student.age);
+
+type Point = {
+  x: number;
+  y: number;
+};
+let xx: Point = { x: 100, y: 100 };
+
+/*-------------------------------------------------------------------*/
+// Interfaces
+interface Manager {
+  name: string;
+  subordiates?: number;
+}
+let storeManager: Manager = {
+  name: 'Bilal',
+};
+
+interface Point1 {
+  x: number;
+  y: number;
+}
+let yy: Point1 = { x: 100, y: 100 };
+
+/*-------------------------------------------------------------------*/
