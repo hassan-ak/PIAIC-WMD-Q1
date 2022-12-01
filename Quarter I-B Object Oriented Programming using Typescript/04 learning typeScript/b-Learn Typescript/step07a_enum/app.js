82411e8498c41fa4681fad53e63d66e8c1c837ff
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-12-01 06:25:19 +0500

    step07a_enum

--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step07a_enum/app.js
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step07a_enum/app.js
@@ -1,4 +1,42 @@
 "use strict";
+// step07a_enum
+/*****************************************************/
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+    Direction[Direction["Left"] = 3] = "Left";
+    Direction[Direction["Right"] = 4] = "Right";
+})(Direction || (Direction = {}));
+/*****************************************************/
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+/*****************************************************/
+var UserResponse;
+(function (UserResponse) {
+    UserResponse[UserResponse["No"] = 0] = "No";
+    UserResponse[UserResponse["Yes"] = 1] = "Yes";
+})(UserResponse || (UserResponse = {}));
+/*****************************************************/
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "UP";
+    Direction2["Down"] = "DOWN";
+    Direction2["Left"] = "LEFT";
+    Direction2["Right"] = "RIGHT";
+})(Direction2 || (Direction2 = {}));
+/*****************************************************/
+var BooleanLikeHeterogeneousEnum;
+(function (BooleanLikeHeterogeneousEnum) {
+    BooleanLikeHeterogeneousEnum[BooleanLikeHeterogeneousEnum["No"] = 0] = "No";
+    BooleanLikeHeterogeneousEnum["Yes"] = "YES";
+})(BooleanLikeHeterogeneousEnum || (BooleanLikeHeterogeneousEnum = {}));
+/*****************************************************/
 var Color;
 (function (Color) {
     Color[Color["Red"] = 0] = "Red";
@@ -6,6 +44,7 @@ var Color;
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {})); //starts with 0
 var c = Color.Green;
+/*****************************************************/
 var Color1;
 (function (Color1) {
     Color1[Color1["Red"] = 1] = "Red";
@@ -14,6 +53,7 @@ var Color1;
 })(Color1 || (Color1 = {}));
 var colorName = Color1[2];
 console.log(colorName);
+/*****************************************************/
 var Color2;
 (function (Color2) {
     Color2[Color2["Red"] = 1] = "Red";
@@ -22,3 +62,4 @@ var Color2;
 })(Color2 || (Color2 = {})); //can assign values to all
 var colorIndex = Color2['Blue'];
 console.log(colorIndex);
+/*****************************************************/
