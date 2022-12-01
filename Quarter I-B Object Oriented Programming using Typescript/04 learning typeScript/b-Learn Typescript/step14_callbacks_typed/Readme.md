43a3644ce2e1a001dd803af3c484b5ce2a3788ab
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-12-02 03:06:43 +0500

    delete CallBacks Typed

--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step14_callbacks_typed/Readme.md
+++
@@ -1,3 +0,0 @@
-# TypeScript
-
-## CallBacks Typed
--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step14_callbacks_typed/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function myCallBack(text) {
-    console.log('inside myCallback ' + text);
-}
-function callingFunction(initialText, callback) {
-    callback(initialText);
-}
-callingFunction('myText', myCallBack);
