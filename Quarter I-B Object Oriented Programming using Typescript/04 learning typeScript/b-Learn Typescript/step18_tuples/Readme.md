93d24b9673bd4cb28c3fb1feebb0bdc336e93554
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-12-02 03:33:57 +0500

    delete Tuples

--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step18_tuples/Readme.md
+++
@@ -1,3 +0,0 @@
-# TypeScript
-
-## Tuples
--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step18_tuples/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//Tuple types have the advantage that you can accurately describe the type of an array of mixed types
-var tuple = [1, 'bob'];
-var secondElement = tuple[1]; // secondElement now has type 'string'
