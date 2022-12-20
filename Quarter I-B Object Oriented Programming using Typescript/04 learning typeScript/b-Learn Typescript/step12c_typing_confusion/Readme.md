5d4e2a2625e028daf7ff279fa9b834cad3f59b84
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-12-20 11:56:39 +0500

    step12c_typing_confusion

---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step12c_typing_confusion/Readme.md
@@ -0,0 +1,11 @@
+# TypeScript
+
+## step12c_typing_confusion
+
+- https://github.com/microsoft/TypeScript/issues/5303
+- microsoft/TypeScript#5303
+- https://www.typescriptlang.org/docs/handbook/type-compatibility.html
+- http://www.typescriptlang.org/Handbook#classes-classes
+- http://basarat.gitbooks.io/typescript/content/docs/classes.html
+- https://www.tektutorialshub.com/typescript/structural-typing-duck-typing-in-typescript/
+- https://imfaber.me/typescript-structural-typing/
---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step12c_typing_confusion/app.js
@@ -0,0 +1,68 @@
+"use strict";
+/********************************/
+/*** step12c_typing_confusion ***/
+/********************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+/************************************************/
+//----
+//A confusion about Structural Typing:
+//Please Read https://github.com/Microsoft/TypeScript/issues/5303
+//Object literals have an associated freshness before they're
+//bound to a variable.
+//The same doesn't apply for any other expressions.
+//The idea is that object literals are often used for option bags
+//(where the user has to manually type out each property
+//name at each use-site), and this behavior catches typos.
+//If you'd like to prevent Animal from being assigned to a Human,
+//you can add a private property to Animal,
+//since private and protected properties need to originate from
+//the same declaration to be compatible.
+//----
+class Human {
+    constructor(name) {
+        this.name = name;
+    }
+}
+//----
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    eat(quantity) {
+        console.log(this.name + ' is a Human and is eating');
+    }
+}
+//----
+let d = { name: 'Hello', age: 4 };
+let h = { name: 'hello' }; // This is allowed
+let h1 = d; // Concept of stale object applied here
+//----
+//Its false, because we have assigned object literal and
+//assignment works because of structural typing
+let isHuman = h instanceof Human;
+console.log(isHuman); // false
+//----
+//This is not allowed need to add all properties
+//including functions
+// fresh object assignment
+// properties or methods missing
+// let a1: Animal = { name: 'Cat', age: 4 };
+//----
+// This below statement is allowed
+// Note there is no parameter in function and it does not give
+// any compilation error but you can not call
+// this function without paramter, see the call below
+let a2 = {
+    name: 'Dog',
+    age: 4,
+    eat: function () {
+        console.log('Eat in object literal');
+    },
+};
+// Maps to class methods need some argument
+// a2.eat(); // Not allowed
+a2.eat(3); // Works fine
+//----
+let isAnimal = a2 instanceof Animal;
+console.log(isAnimal); // What will be printed?
