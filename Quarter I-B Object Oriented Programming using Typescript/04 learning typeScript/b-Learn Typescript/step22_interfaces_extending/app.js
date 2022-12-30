4b3d0885f6e8be0f4550b75d5f587a7de228ba5b
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-12-30 17:20:15 +0500

    step22_interfaces_extending

---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step22_interfaces_extending/app.js
@@ -0,0 +1,13 @@
+"use strict";
+/***********************************/
+/*** step22_interfaces_extending ***/
+/***********************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+var square = {};
+square.color = 'blue';
+square.sideLength = 10;
+//Alternative syntax for casting
+var square1 = {};
+square1.color = 'blue';
+square1.sideLength = 10;
+square1.penWidth = 5.0;
--- Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step22_interfaces_extending/app.ts
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step22_interfaces_extending/app.ts
@@ -1,27 +1,32 @@
+/***********************************/
+/*** step22_interfaces_extending ***/
+/***********************************/
+
+/************************************************/
 interface Shape {
   color: string;
 }
-
 interface Square extends Shape {
   sideLength: number;
 }
-
 var square = <Square>{};
 square.color = 'blue';
 square.sideLength = 10;
-// square.a = 5; //Error
+// Error
+// square.a = 5;
 
+/************************************************/
 //An interface can extend multiple interfaces, creating a combination of all of the interfaces:
-
 interface PenStroke {
   penWidth: number;
 }
-
 interface Square1 extends Shape, PenStroke {
   sideLength: number;
 }
-
-var square1 = {} as Square1; //Alternative syntax for casting
+//Alternative syntax for casting
+var square1 = {} as Square1;
 square1.color = 'blue';
 square1.sideLength = 10;
 square1.penWidth = 5.0;
+
+export {};
