21b60312c4ddf47975f6c993c987459a4857acf8
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-12-30 17:30:30 +0500

    step24_classes_as_interfaces

---
+++ Quarter I-B Object Oriented Programming using Typescript/04 learning typeScript/b-Learn Typescript/step24_classes_as_interfaces/app.js
@@ -0,0 +1,12 @@
+"use strict";
+/************************************/
+/*** step24_classes_as_interfaces ***/
+/************************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+/************************************************/
+// A class declaration creates two things:
+// a type representing instances of the class and a constructor function.
+// Because classes create types, you can use them in the same places you would be able to use interfaces.
+class Point {
+}
+var point3d = { x: 1, y: 2, z: 3 };
